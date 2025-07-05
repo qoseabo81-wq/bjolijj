e1f1dd0a3bb9155aeaca1c0ba70038ad847bc45b
monkybama <tsabyhshms@gmail.com>
2025-07-05 14:17:39 +0200

    Update customrankcard.js

--- scripts/cmds/customrankcard.js
+++ scripts/cmds/customrankcard.js
@@ -5,7 +5,7 @@ const { uploadImgbb } = global.utils;
 
 module.exports = {
 	config: {
-		name: "customrankcard",
+		name: "تصنيف",
 		aliases: ["crc", "customrank"],
 		version: "1.12",
 		author: "NTKhang",
