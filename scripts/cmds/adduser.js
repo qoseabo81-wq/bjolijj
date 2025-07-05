f60b710daa5bbcb88e7ae03c6c79b8e453333068
monkybama <tsabyhshms@gmail.com>
2025-07-05 10:06:21 +0200

    Update adduser.js

--- scripts/cmds/adduser.js
+++ scripts/cmds/adduser.js
@@ -3,7 +3,7 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = {
 	config: {
-		name: "adduser",
+		name: "اضف",
 		version: "1.5",
 		author: "NTKhang",
 		countDown: 5,
