bf2c3e53794870777b9aff6d9844af5acb9f5eba
monkybama <tsabyhshms@gmail.com>
2025-07-11 22:53:37 +0200

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 
 module.exports = {
 	config: {
-		name: "restart",
+		name: "رست",
 		version: "1.1",
 		author: "NTKhang",
 		countDown: 5,
@@ -23,7 +23,7 @@ module.exports = {
 			restartting: "🔄 | Đang khởi động lại bot..."
 		},
 		en: {
-			restartting: "🔄 | Restarting bot..."
+			restartting: "🔄 | اعادة تشغيل نازي ..."
 		}
 	},
 
@@ -31,7 +31,7 @@ module.exports = {
 		const pathFile = `${__dirname}/tmp/restart.txt`;
 		if (fs.existsSync(pathFile)) {
 			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
+			api.sendMessage(`✅ | نازي تمت اعادة التشغيل \n⏰ | الوقت: ${(Date.now() - time) / 1000}s`, tid);
 			fs.unlinkSync(pathFile);
 		}
 	},
